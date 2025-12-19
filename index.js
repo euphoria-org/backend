d1a55fd8d4478955bc58fc21a9eeb0349d7e7843
arpanmehta05 <arpanmehta05@gmail.com>
2025-12-19 21:44:56 +0530

    feat: Add allowed origin for Azure Static Web Apps

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
+  "https://delightful-bay-07530dc00.4.azurestaticapps.net",
 ];
 
 if (process.env.FRONTEND_URL) {
